6d51076c53f75c7d7d7dc5babbf680847db26139
Pedro Henrique <pedrohdavelar@gmail.com>
2024-07-11 01:08:25 -0300

    V1.0
    Finalização das funcionalidades básicas:
    - implementação do menu
    - função de escrita do vencedor no arquivo vencedores.txt
    - impressão dos vencedores do arquivo vencedores.txt por meio da opcão 2 do menu
    - correção de pequeno bug na contagem das pedras
    - adição de chamadas as funções system("pause") e system("cls") para melhorar a cadencia e visibilidade do programa
    -finalização da apresentação

--- bingo.cpp
+++ bingo.cpp
@@ -19,6 +19,7 @@ Autores:
 #include <vector>
 #include <stdlib.h>
 #include <ctype.h>
+#include <fstream>
 
 using namespace std;
 
@@ -50,6 +51,11 @@ void imprime_numeros_sorteados(int n_sorteados[], int n_pedras);
 void avalia_linha(jogador_bingo& jogador, int linha, int n_pedras, int n_sorteados[]);
 //#8 VERIFICA: verifica coluna da cartela
 void avalia_coluna(jogador_bingo& jogador, int coluna, int n_pedras, int n_sorteados[]);
+//#9 Lista os vencedores do arquivo vencedores.txt
+void imprime_vencedores ();
+//#10 Imprime todas as cartelas
+void imprime_todas_cartelas ();
+
 
 //Array para armazenar os numeros préviamente sorteados, variável para armazenar o numero sorteado e flag para determinar se o número sorteado é válido (não é repetido)
 int numeros_sorteados[NUM_PEDRAS];
@@ -58,79 +64,140 @@ bool numero_sorteado_valido = false;
 
 //Começo do programa
 int main() {
+	srand((unsigned) time(NULL)); // gerador de semente aleatoria
 	// Declara vetor de jogadores e variável para armazenar a quantidade de jogadores
 	vector<jogador_bingo> jogadores;
 	int num_jogadores = 0;
+	int menu;
+	bool teve_ganhador = false;
 
 	// Abertura do jogo
 	cout << "\n>> ---------------------------------------------- <<\n";
 	cout << "\nBem Vindo(a) ao Bingo da Computacao para Engenharia!\n";
 	cout << "\n>> ---------------------------------------------- <<\n" << endl;
-	cout << "Para comecar, precisamos que cada jogador informe seus dados e preencha sua cartela." << endl;
+	
+	//loop principal
+	while(true){
+		cout << "Opcoes: (1)Novo Jogo (2)Lista de Vencedores (3)Sair" << endl;
+		cin >> menu;
+		switch (menu){
+			case 1:
+				
+				//limpeza do array contendo as pedras sorteadas
+				for (int x = 0; x < NUM_PEDRAS; x++){
+					numeros_sorteados[x] = 0;
+				}
+				
+				system("cls");
+				teve_ganhador = false;
+				cout << "Para comecar, precisamos que cada jogador informe seus dados e preencha sua cartela." << endl;
 
-	// Solicita o número de jogadores
-	while (num_jogadores < 1){
-		cout << "\nDigite o numero de jogadores: ";
-		cin >> num_jogadores;
-		if (num_jogadores < 1){
-			cout << "Digite um numero valido! O numero de jogadores precisa ser maior ou igual a 1.";
+				num_jogadores = 0; //inicializacao desta variavel para cada loop
+
+				// Solicita o número de jogadores
+				while (num_jogadores < 1){
+					cout << "\nDigite o numero de jogadores: ";
+					cin >> num_jogadores;
+					if (num_jogadores < 1){
+						cout << "Digite um numero valido! O numero de jogadores precisa ser maior ou igual a 1.";
+					}
+				}
+
+				// Redimensiona o vetor de jogadores com base no número fornecido
+				jogadores.resize(num_jogadores);
+
+				// Preenche os dados de cada jogador
+				for (int i = 0; i < num_jogadores; i++) {
+    				cout << "\nPreencha os dados do jogador " << i + 1 << "..." << endl;
+    				preenche_cartela(jogadores[i]);
+				}
+				cout << "Todas as cartelas foram preenchidas!" << endl;
+				system("pause");
+				system("cls");
+
+				cout << "Ok! Aqui esta a cartela dos participantes:" << endl;
+				// Imprime as cartelas de cada jogador
+				for (int i = 0; i < num_jogadores; i++) {
+    				imprime_cartela(jogadores[i], numeros_sorteados, 1);
+				}
+				cout << "Pronto? O jogo ja vai comecar!" << endl;
+				system("pause");
+				system("cls");
+				
+				while(teve_ganhador == false){
+					// Para cada pedra sorteada, serao feitas iteracoes para avaliar se houve vencedor
+					for (int i = 0; i < NUM_PEDRAS; i++) {
+    					cout << "\nComecando o sorteio da " << i + 1 << "a pedra!" << endl;
+    					numero_sorteado_valido = false;
+
+    					// Loop para garantir que o numero sorteado nao seja repetido
+    					while (numero_sorteado_valido == false) {
+    				    	//#2 SORTEIA: sorteia um numero aleatorio
+    				    	numero_sorteado = sorteia_numero();
+    				    	if (checa_numero_sorteado_repetido(numeros_sorteados, i, numero_sorteado) == false) {
+    				        	cout << "\nO " << i + 1 << "o numero sorteado eh: " << numero_sorteado << "!!!" << endl;
+    				        	numero_sorteado_valido = true;
+    				        	numeros_sorteados[i] = numero_sorteado;
+    				        	cout << "--------------------------------\n";
+    				        	cout << "\nNumeros sorteados ate o momento:\n" << endl;
+    				        	//#6 IMPRIMI: imprimi os numeros sorteados
+    				        	imprime_numeros_sorteados(numeros_sorteados, i + 1);
+    				        	cout << "--------------------------------\n";
+    				    	}
+    					}
+
+    					// Cinco iteracoes para avaliar as 5 linhas / 5 colunas das cartelas dos jogadores
+    					for (int j = 0; j < 5; j++) {
+    				    	for (int k = 0; k < num_jogadores; k++) {
+    				        	avalia_linha(jogadores[k], j, i+1, numeros_sorteados);
+    				        	avalia_coluna(jogadores[k], j, i+1, numeros_sorteados);
+    				    	}
+    					}
+
+    					// Verifica se algum jogador ganhou
+    					for (int k = 0; k < num_jogadores; k++) {
+    				    	if (jogadores[k].ganhou == true) {
+    				        	cout << "\n!!! BINGO PARA O JOGADOR(A) !!!\n\n\t>>> " << jogadores[k].nome_jogador << " <<<" << endl;
+    				        	imprime_cartela(jogadores[k], numeros_sorteados, i+1);
+								teve_ganhador = true;
+								fstream vencedores;
+								vencedores.open("vencedores.txt", ios::app);
+								if (!vencedores){
+									cout << "Erro critico! Falha ao abrir o arquivo vencedores.txt. Encerrando o programa...";
+									return 1;
+								}
+								vencedores << jogadores[k].nome_jogador << " " << i+1 << endl;
+								vencedores.close();
+								system("pause");
+								system("cls");
+    				    	}
+    					}
+						if (teve_ganhador == false){
+							for (jogador_bingo a : jogadores){             //iteracao do vetor jogadores para impressao de todas as cartelas
+								imprime_cartela(a, numeros_sorteados, i+1);
+							}		
+							system("pause");
+							system("cls");
+						}
+					if (teve_ganhador == true){
+						break;
+						}
+					}
+				}
+				break;
+			case 2:
+				imprime_vencedores();
+				system("pause");
+				system("cls");
+				break;
+			case 3:
+				system("cls");
+				cout << "Muito obrigado por jogar! Ateh a proxima...";
+				return 0;
+			default:
+				cout << "Opcao invalida. Repita por favor." << endl;
 		}
 	}
-
-	// Redimensiona o vetor de jogadores com base no número fornecido
-	jogadores.resize(num_jogadores);
-
-	// Preenche os dados de cada jogador
-	for (int i = 0; i < num_jogadores; i++) {
-    	cout << "\nPreencha os dados do jogador " << i + 1 << "..." << endl;
-    	preenche_cartela(jogadores[i]);
-	}
-
-	// Imprime as cartelas de cada jogador
-	for (int i = 0; i < num_jogadores; i++) {
-    	imprime_cartela(jogadores[i], numeros_sorteados, 1);
-	}
-
-	// Para cada pedra sorteada, serao feitas iteracoes para avaliar se houve vencedor
-	for (int i = 0; i < NUM_PEDRAS; i++) {
-    	cout << "\nComecando o sorteio da " << i + 1 << "a pedra!" << endl;
-    	numero_sorteado_valido = false;
-
-    	// Loop para garantir que o numero sorteado nao seja repetido
-    	while (numero_sorteado_valido == false) {
-        	//#2 SORTEIA: sorteia um numero aleatorio
-        	numero_sorteado = sorteia_numero();
-        	if (checa_numero_sorteado_repetido(numeros_sorteados, i, numero_sorteado) == false) {
-            	cout << "\nO " << i + 1 << "o numero sorteado eh: " << numero_sorteado << "!!!" << endl;
-            	numero_sorteado_valido = true;
-            	numeros_sorteados[i] = numero_sorteado;
-            	cout << "--------------------------------\n";
-            	cout << "\nNumeros sorteados ate o momento:\n" << endl;
-            	//#6 IMPRIMI: imprimi os numeros sorteados
-            	imprime_numeros_sorteados(numeros_sorteados, i + 1);
-            	cout << "--------------------------------\n";
-        	}
-    	}
-
-    	// Cinco iteracoes para avaliar as 5 linhas / 5 colunas das cartelas dos jogadores
-    	for (int j = 0; j < 5; j++) {
-        	for (int k = 0; k < num_jogadores; k++) {
-            	avalia_linha(jogadores[k], j, i, numeros_sorteados);
-            	avalia_coluna(jogadores[k], j, i, numeros_sorteados);
-        	}
-    	}
-
-    	// Verifica se algum jogador ganhou
-    	for (int k = 0; k < num_jogadores; k++) {
-        	if (jogadores[k].ganhou == true) {
-            	cout << "\n!!! BINGO PARA O JOGADOR(A) !!!\n\n\t>>> " << jogadores[k].nome_jogador << " <<<" << endl;
-            	imprime_cartela(jogadores[k], numeros_sorteados, i);
-            	return 0;
-        	}
-    	}		
-		system("pause");
-	}
-	return 0;
 }
 
 //#1 PREENCHE: funcao para preencher o nome do jogador e os numeros da cartela
@@ -232,7 +299,7 @@ bool checa_numero_cartela_repetido(jogador_bingo& jogador, int numero) {
 
 //#4 IMPRIMI: imprime a cartela do jogador
 void imprime_cartela(jogador_bingo& jogador, int n_sorteados[], int n_pedras) {
-	cout << "\nCartela gerada para: " << jogador.nome_jogador << "\n" << endl;
+	cout << "\nCartela do jogador: " << jogador.nome_jogador << "\n" << endl;
 	for (int i = 0; i < 5; i++) {
     	cout << "|";
     	for (int j = 0; j < 5; j++) {
@@ -303,3 +370,50 @@ void avalia_coluna(jogador_bingo& jogador, int coluna, int n_pedras, int n_sorte
     	jogador.ganhou = true;
 	}
 }
+
+//#9 Lista os vencedores do arquivo vencedores.txt
+void imprime_vencedores(){
+	system("cls");
+	ifstream vencedores("vencedores.txt");  //abertura do arquivo vencedores.txt e teste para ver se o arquivo foi aberto com sucesso
+	if (!vencedores){
+		cout << "Erro ao abrir a lista dos vencedores... Você jah jogou alguma rodada? Voltando ao menu..." << endl;
+		return;
+	}
+
+	// struct para montagem do vetor
+	struct vencedor{
+		string nome;
+		int pontos;
+	};
+
+	vencedor v;
+
+	// vetor para armazenar a lista dos vencedores
+	vector<vencedor> lista_vencedores;
+	while (vencedores >> v.nome >> v.pontos){
+		lista_vencedores.push_back(v);
+	}
+	//apos carregar o vetor, fechamento do arquivo
+	vencedores.close();
+
+	//bubble sort para ordenar os vencedores por ordem de menos pedras para conseguir bingo
+	bool numero_foi_trocado = false;
+	do {
+		numero_foi_trocado = false;
+		for (long long unsigned int i = 0; i < (lista_vencedores.size() - 1 ); i++){
+			if (lista_vencedores[i].pontos > lista_vencedores[i+1].pontos){
+				swap(lista_vencedores[i], lista_vencedores[i+1]);
+				numero_foi_trocado = true;
+			}
+		}
+	} while (numero_foi_trocado == true);
+
+	//impressao da lista ordenada dos vencedores
+	cout << "Ranking dos Vencedores!!!" << endl;
+	for (vencedor a : lista_vencedores){
+		int p = 1;
+		cout << "#" << p << ": " << a.nome << " - " << a.pontos << " pontos" << endl;
+		p++;
+	}
+}
+
